da0f92c76740000a0f0aa717e41889233b01b13a
Eric Phung <esphung@iupui.edu>
2015-05-21 23:04:52 -0400

    Eric Phung init commit

--- Drum Machine/MetronomeModel.swift
+++ Drum Machine/MetronomeModel.swift
@@ -3,29 +3,43 @@
 //  Drum Machine
 //
 //  Created by Nicholas Kraftor on 12/6/14.
+//  Edited by Eric Scott Phung (2015.05.21)
 //  Copyright (c) 2014 Enterprise Holdings Inc. All rights reserved.
 //
 
 import Foundation
 
 class MetronomeModel: NSObject {
-    
-    var delegate: MetronomeDelegate?
-    var timer: NSTimer;
-    
-    override init() {
-        self.timer = NSTimer();
-        super.init()
-    }
+	// instance vars
+	var delegate: MetronomeDelegate?
+	var timer: NSTimer;
 
-    func startMetronome() {
-        self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("fireTick"), userInfo: nil, repeats: true)
-    }
-    
-    func fireTick(){
-        println("test in model")
-        delegate?.metronomeTick()
-    }
-    
-    
-}
+	override init() {
+	    self.timer = NSTimer();
+	    super.init()
+	} // end self abstraction
+
+	// start metronome
+	func startMetronome() -> Bool{
+		// int is seconds in interval
+		// default bpm is 60 (@ 1)
+		self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("fireTick"), userInfo: nil, repeats: true)
+		println("Metronome Started")
+		return true;
+	} // end start metronome
+
+	// stop metronome
+	func stopMetronome() -> Bool {
+		self.timer.invalidate()
+		println("Metronome Stopped")
+		return false;
+	} // end stop metronome
+
+	// console log tick
+	func fireTick(){
+		println("tick")
+		delegate?.metronomeTick()
+	} // end fire tick
+
+
+} // end metronome model class definition
--- Drum Machine/MetronomeViewController.swift
+++ Drum Machine/MetronomeViewController.swift
@@ -3,6 +3,7 @@
 //  Drum Machine
 //
 //  Created by Nicholas Kraftor on 12/6/14.
+//  Edited by Eric Scott Phung (2015.05.21)
 //  Copyright (c) 2014 Enterprise Holdings Inc. All rights reserved.
 //
 
@@ -12,17 +13,15 @@ import AVFoundation
 
 class MetronomeViewController: UIViewController, MetronomeDelegate {
     
+    
+    // instance vars
     var metronome: MetronomeModel = MetronomeModel()
     var audioPlayer = AVAudioPlayer()
-    
-    override init (){
-        super.init()
-    }
-    
-    required init(coder aDecoder: NSCoder)
-    {
+    @IBOutlet weak var metroBoolLblTxt: UILabel!
+    @IBOutlet weak var metroBoolSwitch: UISegmentedControl!
+    required init(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
-    }
+    } // end initialize
     
     override func viewDidLoad() {
         metronome.delegate = self
@@ -31,15 +30,34 @@ class MetronomeViewController: UIViewController, MetronomeDelegate {
         audioPlayer = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
         audioPlayer.prepareToPlay()
 
-    }
+    } // end view ready function
 
     func metronomeTick() {
-        println("test in controller")
+        //println("test in controller")
         audioPlayer.play()
         
-    }
+    } // end metronome output audio
     
-    @IBAction func startButtonPressed(sender: UIButton) {
-        self.metronome.startMetronome()
-    }
-}
+    
+    
+    
+    @IBAction func metroSwitchAction(sender: UISegmentedControl) {
+        switch metroBoolSwitch.selectedSegmentIndex{
+        case 0:
+            metroBoolLblTxt.text = "Off";
+            self.metronome.stopMetronome()
+        case 1:
+            metroBoolLblTxt.text = "On";
+            self.metronome.startMetronome()
+        default:
+            break;
+        } // end case switch metronome
+    }// end metro switch action
+
+
+    
+    
+    
+    
+    
+} // end metronome vc class def
